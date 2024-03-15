5008c1e34fab89b875566e20e8c9d05367688418
akhilk49 <akhilkkurian49@gmail.com>
2024-03-15 14:27:32 +0530

    Created Navbar and other components related to it

--- frontend/Auto-Sentry/src/App.jsx
+++ frontend/Auto-Sentry/src/App.jsx
@@ -1,28 +1,27 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home'; 
+import About from './pages/About/About';
+import Services from './pages/Services/Services';
+import Contact from './pages/Contact/Contact';
+import SignUp from './pages/SignUP/SignUP';
+import SignIN from './pages/SignIn/SignIN'
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="container">
-      <header>
-        <h1>Welcome to AutoSentry</h1>
-      </header>
-      <main>
-        <p>
-          AutoSentry is a web application designed to assist vehicle owners in managing and tracking their vehicle's maintenance activities effectively. The primary goal of this project is to provide users with a platform where they can monitor their vehicle's maintenance schedule, track service history, receive reminders for upcoming maintenance tasks, and optimize their vehicle's performance and longevity.
-        </p>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            Click me! Count is {count}
-          </button>
-        </div>
-      </main>
-      <footer>
-        <p> 2024 AutoSentry.</p>
-      </footer>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact-us" element={<Contact />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/signin" element={<SignIN />} />
+      </Routes>
+    </Router>
   );
 }
 
