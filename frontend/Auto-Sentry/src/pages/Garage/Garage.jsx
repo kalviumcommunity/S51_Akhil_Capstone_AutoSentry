b11485a74bc3d7038e20d4abc8d91486ec23db10
akhilk49 <akhilkkurian49@gmail.com>
2024-04-26 14:30:53 +0530

    Add loader component for seamless data fetching and display

--- frontend/Auto-Sentry/src/pages/Garage/Garage.jsx
+++ frontend/Auto-Sentry/src/pages/Garage/Garage.jsx
@@ -7,14 +7,15 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./Garage.css";
-import RedirectService from "./redirectService"; // Import the RedirectService function
+import RedirectService from "./redirectService";
 
 const Garage = () => {
   const [vehicles, setVehicles] = useState([]);
-  const { user } = useAuth0();
+  const [loading, setLoading] = useState(true); // State to manage loading
+  const { user, isAuthenticated } = useAuth0();
 
   useEffect(() => {
-    if (user && user.nickname) {
+    if (isAuthenticated && user && user.nickname) {
       axios
         .get("http://localhost:5000/api/vehicles")
         .then((response) => {
@@ -25,9 +26,12 @@ const Garage = () => {
         })
         .catch((error) => {
           console.error("Error fetching data:", error);
+        })
+        .finally(() => {
+          setLoading(false); // Set loading to false whether data fetch succeeds or fails
         });
     }
-  }, [user]);
+  }, [user, isAuthenticated]);
 
   const handleDelete = (id) => {
     axios
@@ -43,61 +47,78 @@ const Garage = () => {
   };
 
   return (
-    <div className="garage-container">
-      <h1>My Garage</h1>
-      <ToastContainer />
-      <div className="vehicle-list">
-        {vehicles.map((vehicle) => (
-          <div key={vehicle._id} className="vehicle-card">
-            <img src={vehicle.image} alt="Vehicle" />
-            <div className="vehicle-details">
-              <h3>
-                {vehicle.make} {vehicle.model}
-              </h3>
-              <p>Year: {vehicle.year}</p>
-              <p>Modification: {vehicle.modification}</p>
-              <div className="control-btns">
-                <NavLink
-                  to={`/update/${vehicle._id}`}
-                  className="update"
-                  activeClassName="active"
-                >
-                  <FaPen />
-                </NavLink>
-                <a
-                  className="btn-danger"
-                  onClick={(e) => handleDelete(vehicle._id)}
-                >
-                  <MdDelete />
-                </a>
-              </div>
-              <div className="activ-btns">
-                <NavLink to={`/maintenancetasks`}>
-                  <button className="btn-mt">Maintenance tasks</button>
-                </NavLink>
-                <NavLink to={`/servicehistory`}>
-                  <button className="btn-mt">Service History</button>
-                </NavLink>
-                <button
-                  className="btn-mt"
-                  onClick={() => RedirectService(vehicle.make)}
-                >
-                  Book Service
-                </button>
-                <NavLink to={`/servicehistory`}>
-                  <button className="btn-mt">Empty</button>
-                </NavLink>
-              </div>
-            </div>
+    isAuthenticated && (
+      <div className="garage-container">
+        <h1>My Garage</h1>
+        <ToastContainer />
+        {loading ? ( // Render the loader when loading is true
+          <div className="loader">
+            <svg className="car" width="102" height="40" xmlns="http://www.w3.org/2000/svg">
+              <g transform="translate(2 1)" stroke="#002742" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
+                <path className="car__body" d="M47.293 2.375C52.927.792 54.017.805 54.017.805c2.613-.445 6.838-.337 9.42.237l8.381 1.863c2.59.576 6.164 2.606 7.98 4.531l6.348 6.732 6.245 1.877c3.098.508 5.609 3.431 5.609 6.507v4.206c0 .29-2.536 4.189-5.687 4.189H36.808c-2.655 0-4.34-2.1-3.688-4.67 0 0 3.71-19.944 14.173-23.902zM36.5 15.5h54.01" stroke-width="3"/>
+                <ellipse className="car__wheel--left" stroke-width="3.2" fill="#FFF" cx="83.493" cy="30.25" rx="6.922" ry="6.808"/>
+                <ellipse className="car__wheel--right" stroke-width="3.2" fill="#FFF" cx="46.511" cy="30.25" rx="6.922" ry="6.808"/>
+                <path className="car__line car__line--top" d="M22.5 16.5H2.475" stroke-width="3"/>
+                <path className="car__line car__line--middle" d="M20.5 23.5H.4755" stroke-width="3"/>
+                <path className="car__line car__line--bottom" d="M25.5 9.5h-19" stroke-width="3"/>
+              </g>
+            </svg>
           </div>
-        ))}
-        <NavLink to="/addnew" className="addnew" activeClassName="active">
-          <FaPlus />
-          <h2>Add New Car</h2>
-        </NavLink>
+        ) : (
+          <div className="vehicle-list">
+            {vehicles.map((vehicle) => (
+              <div key={vehicle._id} className="vehicle-card">
+                <img src={vehicle.image} alt="Vehicle" />
+                <div className="vehicle-details">
+                  <h3>
+                    {vehicle.make} {vehicle.model}
+                  </h3>
+                  <p>Year: {vehicle.year}</p>
+                  <p>Modification: {vehicle.modification}</p>
+                  <div className="control-btns">
+                    <NavLink
+                      to={`/update/${vehicle._id}`}
+                      className="update"
+                      activeClassName="active"
+                    >
+                      <FaPen />
+                    </NavLink>
+                    <a
+                      className="btn-danger"
+                      onClick={(e) => handleDelete(vehicle._id)}
+                    >
+                      <MdDelete />
+                    </a>
+                  </div>
+                  <div className="activ-btns">
+                    <NavLink to={`/maintenancetasks`}>
+                      <button className="btn-mt">Maintenance tasks</button>
+                    </NavLink>
+                    <NavLink to={`/servicehistory`}>
+                      <button className="btn-mt">Service History</button>
+                    </NavLink>
+                    <button
+                      className="btn-mt"
+                      onClick={() => RedirectService(vehicle.make)}
+                    >
+                      Book Service
+                    </button>
+                    <NavLink to={`/servicehistory`}>
+                      <button className="btn-mt">Empty</button>
+                    </NavLink>
+                  </div>
+                </div>
+              </div>
+            ))}
+            <NavLink to="/addnew" className="addnew" activeClassName="active">
+              <FaPlus />
+              <h2>Add New Car</h2>
+            </NavLink>
+          </div>
+        )}
+        <ToastContainer />
       </div>
-      <ToastContainer />
-    </div>
+    )
   );
 };
 
